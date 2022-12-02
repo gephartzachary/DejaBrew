d390da6105c9e3e6b010cb0f0d8a0c3cb3fc4fee
Elijah Gregor Helfenstein <helfenstein1366@uwlax.edu>
2022-12-02 16:42:25 -0600

    chng

--- DB.sql
+++ DB.sql
@@ -1,5 +1,5 @@
 --
--- File generated with SQLiteStudio v3.3.3 on Fri Dec 2 16:06:21 2022
+-- File generated with SQLiteStudio v3.3.3 on Fri Dec 2 16:30:53 2022
 --
 -- Text encoding used: System
 --
@@ -8,8 +8,8 @@ BEGIN TRANSACTION;
 
 -- Table: Bar
 CREATE TABLE Bar (
-    BarID     INTEGER PRIMARY KEY
-                      DEFAULT ( -1),
+    BarID     INTEGER PRIMARY KEY AUTOINCREMENT
+                      NOT NULL,
     Details   STRING,
     DateAdded DATE,
     Name      STRING,
@@ -21,8 +21,8 @@ CREATE TABLE Bar (
 
 -- Table: Beer
 CREATE TABLE Beer (
-    BeerID    INTEGER PRIMARY KEY
-                      DEFAULT ( -1),
+    BeerID    INTEGER PRIMARY KEY AUTOINCREMENT
+                      NOT NULL,
     DateAdded DATE,
     Type      STRING,
     Cost      DOUBLE,
@@ -35,8 +35,8 @@ CREATE TABLE Beer (
 
 -- Table: Brewery
 CREATE TABLE Brewery (
-    BreweryID INTEGER PRIMARY KEY
-                      DEFAULT ( -1),
+    BreweryID INTEGER PRIMARY KEY AUTOINCREMENT
+                      NOT NULL,
     Name      STRING,
     Type      STRING,
     Picture   STRING,
@@ -48,8 +48,8 @@ CREATE TABLE Brewery (
 
 -- Table: Brews
 CREATE TABLE Brews (
-    BrewID    INTEGER PRIMARY KEY
-                      DEFAULT ( -1),
+    BrewID    INTEGER PRIMARY KEY AUTOINCREMENT
+                      NOT NULL,
     BreweryID INTEGER REFERENCES Brewery (BreweryID),
     BeerID    INTEGER REFERENCES Beer (BeerID) 
 );
@@ -66,8 +66,8 @@ CREATE TABLE Buys (
 
 -- Table: Claim
 CREATE TABLE Claim (
-    ClaimID INTEGER PRIMARY KEY
-                    DEFAULT ( -1),
+    ClaimID INTEGER PRIMARY KEY AUTOINCREMENT
+                    NOT NULL,
     BarID   INTEGER REFERENCES Bar (BarID),
     TabID   INTEGER REFERENCES Tab (TabID) 
 );
@@ -75,8 +75,8 @@ CREATE TABLE Claim (
 
 -- Table: Pay
 CREATE TABLE Pay (
-    PayID INTEGER PRIMARY KEY
-                  DEFAULT ( -1),
+    PayID INTEGER PRIMARY KEY AUTOINCREMENT
+                  NOT NULL,
     TabID INTEGER REFERENCES Tab (TabID),
     PID   INTEGER REFERENCES Person (PID) 
 );
@@ -84,8 +84,8 @@ CREATE TABLE Pay (
 
 -- Table: Person
 CREATE TABLE Person (
-    PID     INTEGER PRIMARY KEY
-                    DEFAULT ( -1),
+    PID     INTEGER PRIMARY KEY AUTOINCREMENT
+                    NOT NULL,
     Name    STRING,
     Picture STRING
 );
@@ -93,16 +93,16 @@ CREATE TABLE Person (
 
 -- Table: Tab
 CREATE TABLE Tab (
-    TabID INTEGER PRIMARY KEY
-                  DEFAULT ( -1),
+    TabID INTEGER PRIMARY KEY AUTOINCREMENT
+                  NOT NULL,
     Total INTEGER
 );
 
 
 -- Table: TabList
 CREATE TABLE TabList (
-    ListID   INTEGER PRIMARY KEY
-                     DEFAULT ( -1),
+    ListID   INTEGER PRIMARY KEY AUTOINCREMENT
+                     NOT NULL,
     BeerID   INTEGER REFERENCES Beer (BeerID),
     TabID    INTEGER REFERENCES Tab (TabID),
     Quantity INTEGER,
@@ -111,3 +111,4 @@ CREATE TABLE TabList (
 
 
 COMMIT TRANSACTION;
+PRAGMA foreign_keys = on;
