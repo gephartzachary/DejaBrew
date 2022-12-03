4a14c729f646b4f826345ef66594aa66949d2654
Elijah Gregor Helfenstein <helfenstein1366@uwlax.edu>
2022-12-03 14:44:45 -0600

    Date dataype fix

--- DB_sql/SeparatePopulates/breweryTablePop.sql
+++ DB_sql/SeparatePopulates/breweryTablePop.sql
@@ -1,12 +1,12 @@
 INSERT INTO Brewery (Name,Type,Picture,Location,DateAdded,Details)
 VALUES
-  ("owner storage","micro","/picture","Milwaukee","09.24.22","fringilla. Donec feugiat metus sit amet ante. Vivamus"),
-  ("lab chemistry","brewpub","/picture","West Jordan","11.27.22","sollicitudin orci sem eget massa. Suspendisse eleifend. Cras"),
-  ("contribution orange","taproom","/picture","Dover","11.21.22","mollis non, cursus non, egestas"),
-  ("idea ratio","regional","/picture","Fort Worth","09.29.22","nec urna suscipit nonummy."),
-  ("grey blue","brewpub","/picture","Norman","10.14.22","eu tellus eu augue"),
-  ("ratio highway","micro","/picture","Chesapeake","09.05.22","aliquet vel, vulputate eu, odio. Phasellus at augue"),
-  ("storage bird","taproom","/picture","Jefferson City","10.29.22","orci. Phasellus dapibus quam"),
-  ("flight appearance","brewpub","/picture","Auburn","09.10.22","pretium neque. Morbi quis urna. Nunc"),
-  ("lab chemistry","taproom","/picture","South Burlington","09.02.22","Duis gravida. Praesent eu nulla at sem molestie"),
-  ("winner emotion","brewpub","/picture","Warren","11.12.22","metus sit amet ante. Vivamus");
+  ("owner storage","micro","/picture","Milwaukee","2022-12-13 17:17:29","fringilla. Donec feugiat metus sit amet ante. Vivamus"),
+  ("lab chemistry","brewpub","/picture","West Jordan","2022-11-11 06:09:23","sollicitudin orci sem eget massa. Suspendisse eleifend. Cras"),
+  ("contribution orange","taproom","/picture","Dover","2022-11-12 18:58:14","mollis non, cursus non, egestas"),
+  ("idea ratio","regional","/picture","Fort Worth","2022-12-07 07:13:30","nec urna suscipit nonummy."),
+  ("grey blue","brewpub","/picture","Norman","2022-12-05 00:05:37","eu tellus eu augue"),
+  ("ratio highway","micro","/picture","Chesapeake","2022-09-02 23:24:12","aliquet vel, vulputate eu, odio. Phasellus at augue"),
+  ("storage bird","taproom","/picture","Jefferson City","2022-09-30 12:35:50","orci. Phasellus dapibus quam"),
+  ("flight appearance","brewpub","/picture","Auburn","2022-11-29 18:16:54","pretium neque. Morbi quis urna. Nunc"),
+  ("lab chemistry","taproom","/picture","South Burlington","2022-12-05 18:00:55","Duis gravida. Praesent eu nulla at sem molestie"),
+  ("winner emotion","brewpub","/picture","Warren","2022-11-05 10:53:53","metus sit amet ante. Vivamus");
