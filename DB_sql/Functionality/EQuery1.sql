0c777c91c67af71527ea6df29e1546f994921431
Elijah Gregor Helfenstein <helfenstein1366@uwlax.edu>
2022-12-03 20:08:30 -0600

    Adjusting Query

--- DB_sql/Functionality/EQuery1.sql
+++ DB_sql/Functionality/EQuery1.sql
@@ -1,4 +1,4 @@
 SELECT Bar.name as Name, Bar.Location as Location FROM Bar
 JOIN Buys JOIN Beer
 ON Bar.BarID = Buys.BarID AND Buys.BeerID = Beer.BeerID
-Where Beer.Name LIKE 'variableName'
+--Where Beer.Name LIKE 'variable Beer.Name = selected Beer'
--- DB_sql/Functionality/EQuery2.sql
+++ DB_sql/Functionality/EQuery2.sql
@@ -2,3 +2,4 @@ Select TabID, sum(Quantity*Cost) AS Price
 FROM Tablist JOIN Beer
 ON TabList.BeerID = Beer.BeerID 
 GROUP BY TabID
+--HAVING TabID = 'variable = TabID from Pay.PID = Person.PID Where PID is from specific Person'
