d8723c21b915a68547506241a328fa2958b6377d
Elijah Gregor Helfenstein <helfenstein1366@uwlax.edu>
2022-12-09 14:46:59 -0600

    last Query

--- DB_sql/Functionality/Query4.sql
+++ DB_sql/Functionality/Query4.sql
@@ -1,5 +1,10 @@
-SELECT Bar.name as Name, count(Beer.BeerID) as Beers FROM Bar
-JOIN Buys JOIN Beer
-ON Bar.BarID = Buys.BarID AND Buys.BeerID = Beer.BeerID
-GROUP BY Bar.BarID
-HAVING Beers > ?
+SELECT * 
+FROM ( 
+SELECT Bar.BarID as BarID, count(Beer.BeerID) as Beers FROM Bar 
+	JOIN Buys JOIN Beer 
+	ON Bar.BarID = Buys.BarID AND Buys.BeerID = Beer.BeerID 
+	GROUP BY Bar.BarID 
+	HAVING Beers > ? 
+    ) as BeerAmount JOIN Bar 
+ON Bar.BarID = BeerAmount.BarID
+WHERE Name LIKE ?
