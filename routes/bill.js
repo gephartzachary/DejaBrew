c91fa06f3aa4e8435f1615a9efd71c6142f3ff22
Elijah Gregor Helfenstein <helfenstein1366@uwlax.edu>
2022-12-08 18:19:20 -0600

    add beer

--- routes/bill.js
+++ routes/bill.js
@@ -15,7 +15,16 @@ billRouter.use(session({
 billRouter.use(flash());
 
 billRouter.get("/bill", function(req, res, next) {
-    var selectBarSQL = 'SELECT * FROM Bar';
+    var selectBarSQL = "SELECT Person.Name as Name, Tab.TabID as TabID, Tab.DateAdded as Date, Tabs.Price as Price "
+    +"FROM ( "
+        +"SELECT Tablist.TabID as TabID, sum(Quantity*Cost) AS Price "
+        +"FROM Tablist JOIN Beer "
+        +"ON TabList.BeerID = Beer.BeerID "
+        +"GROUP BY TabID "
+    +") as Tabs "
+    +"JOIN Tab JOIN Pay JOIN Person "
+    +"ON Tabs.TabID = Tab.TabID AND Tabs.TabID = Pay.TabID AND Pay.PID = Person.PID "
+    +"ORDER BY Name ASC, Date ASC, Price DESC";
 
     db.query(selectBarSQL, function(err, result, fields) {
         if (err) {throw err}
