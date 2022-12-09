e490b348c4b5e79e15582959bbd419164ac5fef4
Zach Gephart <gephartzachary@gmail.com>
2022-12-08 21:10:21 -0600

    Created edit and delete fields

--- routes/beer.js
+++ routes/beer.js
@@ -21,9 +21,62 @@ beerRouter.get("/beer", function(req, res, next) {
         if (err) {throw err}
         res.render("beer", {
             beerData: result,
-            beerChange: req.flash("beerChange")
+            beerChange: req.flash("beerChanged")
         });
     });
 });
+
+beerRouter.post("/beer-like", function(req, res) {
+    var beerID = req.body.beerID;
+    var newLikes = req.body.Likes + 1;
+
+    var incremntBeerSQL = "UPDATE BEER SET Likes = ? WHERE BeerID = ? ";
+
+    db.query(incremntBeerSQL, [newLikes, beerID], function (err, result, fields) {
+        if (err) {throw err}
+
+        req.flash("beerChanged", "Beer liked");
+        res.redirect("/beer");
+    });
+});
+
+beerRouter.post("/beer-dislike", function(req, res) {
+    var beerID = req.body.beerID;
+    var newDislikes = req.body.Dislikes + 1;
+
+    var decremntBeerSQL = "UPDATE BEER SET Likes = ? WHERE BeerID = ? ";
+
+    db.query(decremntBeerSQL, [newDislikes, beerID], function (err, result, fields) {
+        if (err) {throw err}
+
+        req.flash("beerChanged", "Beer disliked");
+        res.redirect("/beer");
+    });
+});
+
+beerRouter.post("/beer-remove", function(req, res) {
+
+    var removeBeer = req.body.beerID;
+
+    var removeBuysJOIN = 'DELETE FROM Buys WHERE BeerID = ? ';
+    var removeBrewsJOIN = 'DELETE FROM Brews WHERE BeerID = ? ';
+    var removeBeerSQL = 'DELETE FROM Beer WHERE BeerID = ? ';
+
+    db.query(removeBuysJOIN, [removeBeer], function (err, result, fields) {
+        if (err) { throw err }
+
+        db.query(removeBrewsJOIN, [removeBeer], function(err, result, fields) {
+            if (err) { throw err }
+
+            db.query(removeBeerSQL, [removeBeer], function (err, result, fields) {
+                if (err) { throw err}
+
+                req.flash("beerChanged", "Beer removed");
+                res.redirect("/beer");
+            });
+        });
+    });
+});
+
 
 module.exports = beerRouter;
