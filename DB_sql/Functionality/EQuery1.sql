1c7a10fd8f4678064702c3aacf9f0fbe2494f537
Elijah Gregor Helfenstein <helfenstein1366@uwlax.edu>
2022-12-08 23:39:05 -0600

    push

--- DB_sql/Functionality/EQuery1.sql
+++ DB_sql/Functionality/EQuery1.sql
@@ -1,4 +1,5 @@
-SELECT Beer.name as Name, count(Bar.BarID) as Bars FROM Bar
+SELECT Bar.name as Name, count(Beer.BeerID) as Beers FROM Bar
 JOIN Buys JOIN Beer
 ON Bar.BarID = Buys.BarID AND Buys.BeerID = Beer.BeerID
-GROUP BY Beer.BeerID
+GROUP BY Bar.BarID
+HAVING Beers > ?
