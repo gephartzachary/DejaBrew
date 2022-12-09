0878886f23b030fcc10c54009d74517908a6e9e3
Elijah Gregor Helfenstein <helfenstein1366@uwlax.edu>
2022-12-08 23:25:02 -0600

    updates

--- DB_sql/Functionality/EQuery1.sql
+++ DB_sql/Functionality/EQuery1.sql
@@ -1,4 +1,4 @@
-SELECT Bar.name as Name, Bar.Location as Location FROM Bar
+SELECT Beer.name as Name, count(Bar.BarID) as Bars FROM Bar
 JOIN Buys JOIN Beer
 ON Bar.BarID = Buys.BarID AND Buys.BeerID = Beer.BeerID
---Where Beer.Name LIKE 'variable Beer.Name = selected Beer'
+GROUP BY Beer.BeerID
